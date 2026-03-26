c0fd356358e4f7f590f60b4f6e1edba67ae4cda0
Bharat Khandelwal <bharatkhandelwal2007@gmail.com>
2026-03-26 22:15:12 +0530

    integrate Tailwind CSS and create Login component

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Login from './components/login/Login'
+
+const App = () => {
+  return (
+    <div className='h-screen w-screen bg-linear-to-r from-purple-600 to-blue-500 text-white text-3xl font-bold flex items-center justify-center'>
+      <Login/>
+    </div>
+  )
+}
+
+export default App
