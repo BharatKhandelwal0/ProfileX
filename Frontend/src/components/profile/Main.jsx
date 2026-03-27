e9e065ca21e1f1f738e9e797c00acee14fb74957
Bharat Khandelwal <bharatkhandelwal2007@gmail.com>
2026-03-27 09:48:12 +0530

    update shadow effect in Main component

--- Frontend/src/components/profile/Main.jsx
+++ Frontend/src/components/profile/Main.jsx
@@ -5,11 +5,10 @@ import Footer from './Footer'
 
 const Main = () => {
   return (
-    <div className='h-[80%] w-[70%] bg-purple-950 text-white text-3xl font-bold rounded-md shadow-lg'>
+    <div className='h-[80%] w-[70%] bg-purple-950 text-white text-3xl font-bold rounded-md shadow-xl/200'>
         <Header/>
         <Middle/>
         <Footer/>
-      
     </div>
   )
 }
