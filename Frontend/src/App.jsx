56727b6b12b68a12494ed1e5e76cfdff72e2bff3
Bharat Khandelwal <bharatkhandelwal2007@gmail.com>
2026-03-28 14:11:46 +0530

    implement login functionality with form handling and user authentication

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,8 +4,8 @@ import Main from './components/profile/Main.jsx'
 const App = () => {
   return (
     <div className='h-screen w-screen bg-linear-to-r from-purple-600 to-blue-500 text-white text-3xl font-bold flex items-center justify-center'>
-      {/* <Login/> */}
-      <Main/>
+      <Login/>
+      {/* <Main/> */}
     </div>
   )
 }
--- Frontend/src/components/login/Login.jsx
+++ Frontend/src/components/login/Login.jsx
@@ -1,14 +1,52 @@
+import { useState } from "react"
+
 const Login = () => {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleSubmit = async (e)=>{
+    e.preventDefault()
+
+    const userData= {
+      name: name,
+      email: email,
+      password: password
+    }
+
+    await registerUser(userData)
+  }
+
+  const registerUser = async (userData)=>{
+    const res = await fetch('http://localhost:1100/user/login',{
+      method:"POST",
+      headers:{
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(userData)
+    })
+    return res.json()
+  }
+
   return (
     <div className='h-[50%] w-[50%] bg-white  text-black text-3xl font-bold flex flex-col items-center justify-center rounded-md shadow-lg'>
+
         <h1 className='mb-10 text-5xl '>Login</h1>
-      <form className='flex flex-col gap-4 w-[70%]'>
-        <input type="text" placeholder='Name' name='name' className='p-2 rounded-md bg-gray-700 text-white focus:outline-none focus:ring-2 focus:ring-slate-600'/>
-        <input type="email" placeholder='Email' name='email' className='p-2 rounded-md bg-gray-700 text-white focus:outline-none focus:ring-2 focus:ring-slate-600'/>
-        <input type="password" placeholder='Password' name='password' className='p-2 rounded-md bg-gray-700 text-white focus:outline-none focus:ring-2 focus:ring-slate-600'/>
+
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4 w-[70%]'>
+
+        <input type="text" placeholder='Name' name='name' value={name} onChange={(e)=>setName(e.target.value)} className='p-2 rounded-md bg-gray-700 text-white focus:outline-none focus:ring-2 focus:ring-slate-600'/>
+
+        <input type="email" placeholder='Email' name='email' value={email} onChange={(e)=>setEmail(e.target.value)} className='p-2 rounded-md bg-gray-700 text-white focus:outline-none focus:ring-2 focus:ring-slate-600'/>
+
+        <input type="password" placeholder='Password' name='password' value={password} onChange={(e)=>setPassword(e.target.value)} className='p-2 rounded-md bg-gray-700 text-white focus:outline-none focus:ring-2 focus:ring-slate-600'/>
+
         <button type='submit' className='bg-linear-to-r from-purple-700 to-blue-600 cursor-pointer text-white font-bold py-2 px-4 rounded-md'>Login</button>
+
       </form>
+      
       <a href="/register" className='text-black mt-7 text-xl'>Don't have an account? <span className='underline text-blue-800  hover:text-blue-900'>Register</span></a>
+
     </div>
   )
 }
